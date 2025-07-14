727712f7b17cc879a3b1a2a71762f11fb7fd74c2
DoPhuc05 <phuc201005@gmail.com>
2025-07-14 14:03:29 +0700

    Phúc update

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,18 +1,22 @@
 package com.example;
 
-import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/hello9")
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của ---feature hello-19.06.2025!</h1>");
+        out.println("<h1>Hello, I am a servlet,chú ý nhánh của ---feature hello-14.07.2025</h1>");
 
     }
 }
