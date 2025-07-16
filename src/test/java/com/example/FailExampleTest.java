46df49118e324ab525f03ace5d0d4bad0b38d7e2
hoangkans <thienkt179@gmail.com>
2025-07-16 17:53:34 +0700

    Thêm file unit test mới luôn luôn fail để kiểm tra CI/CD

---
+++ src/test/java/com/example/FailExampleTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class FailExampleTest {
+    @Test
+    public void testAlwaysFail() {
+        org.junit.jupiter.api.Assertions.fail("Test này luôn luôn fail để kiểm tra CI/CD");
+    }
+}
